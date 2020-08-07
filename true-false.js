49a7b00a35d436cce7225d536da84b2a3d5bac25
darklordsadaf <darklordsadaf@gmail.com>
2020-08-08 02:57:51 +0600

    true vs false

---
+++ true-false.js
@@ -0,0 +1,21 @@
+/** Falsy */
+// 0
+// ""
+// false
+// undefined
+// null
+// NaN
+
+/** Truthy */
+// '0'
+// " "
+// []
+// {}
+
+let name = '0';
+if(name){
+    console.log("Condition is true");
+}
+else{
+    console.log("Condition is false");
+}
