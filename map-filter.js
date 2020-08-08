a6fe16c222bc4151523689e19dbb1b8b625fd2f7
darklordsadaf <darklordsadaf@gmail.com>
2020-08-09 02:06:24 +0600

    map-filter done

---
+++ map-filter.js
@@ -0,0 +1,24 @@
+// const num = [2, 3, 4, 5, 6, 7, 8];
+// const result = num.map(function(element){
+//     return element * element;
+// })
+// console.log(result);
+
+
+
+
+// const num = [2, 3, 4, 5, 6, 7, 8];
+// const result = num.map(x => x * x);
+// console.log(result);
+
+
+
+// const num = [2, 3, 4, 5, 6, 7, 8];
+// const result = num.filter(x => x < 5);
+// console.log(result);
+
+
+
+const num = [2, 3, 4, 5, 6, 7, 8];
+const result = num.find(x => x < 5);
+console.log(result);
